b8172ce4f24ea0cf71954b4d4ecad44f2f039026
Anees Fatima Shaik <Anees@MacBook-Pro.local>
2021-01-25 00:06:50 -0700

    update

--- codility/rotateArray.go
+++ codility/rotateArray.go
@@ -2,46 +2,6 @@ package main
 
 import "fmt"
 
-/*
-An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [4, 9, 10, 8, 7] is [7, 4, 9, 10, 8] (elements are shifted right by one index and 6 is moved to the first place).
-
-The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
-
-funtion to rotate:
-
-func rotate(A []int, K int) []int
-
-that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
-
-For example, given
-
-    A = [] int{4, 9, 10, 8, 7}
-    K = 3
-the function should return [10, 8, 7, 4, 9]. Three rotations were made:
-
-    [4, 9, 10, 8, 7] -> [7, 4, 9, 10, 8]
-    [7, 4, 9, 10, 8] -> [8, 7, 4, 9, 10]
-    [8, 7, 4, 9, 10] -> [10, 8, 7, 4, 9]
-For another example, given
-
-    A = [] int{0, 0, 0}
-    K = 1
-the function should return [0, 0, 0]
-
-Given
-
-    A = [] int {1, 2, 3, 4}
-    K = 4
-the function should return [1, 2, 3, 4]
-
-other factor
-
-N and K are integers within the range [0..100];
-each element of array A is an integer within the range [−1,000..1,000].
-In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
-
-*/
-
 func main() {
 	arr := []int{4, 9, 10, 8, 7}
 	fmt.Println("Before Rotation ::", arr)
