d5dbff2c1400999133986b8f147f1253a391e6a3
Anees Fatima Shaik <Anees@MacBook-Pro.local>
2021-01-24 23:45:26 -0700

    updated

--- codility/cyclicRotation.go
+++ codility/cyclicRotation.go
@@ -7,15 +7,15 @@ An array A consisting of N integers is given. Rotation of the array means that e
 
 The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
 
-Write a function:
+funtion to rotate:
 
-func Solution(A []int, K int) []int
+func rotate(A []int, K int) []int
 
 that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
 
 For example, given
 
-    A = [3, 8, 9, 7, 6]
+    A = [] int{3, 8, 9, 7, 6}
     K = 3
 the function should return [9, 7, 6, 3, 8]. Three rotations were made:
 
@@ -24,13 +24,13 @@ the function should return [9, 7, 6, 3, 8]. Three rotations were made:
     [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
 For another example, given
 
-    A = [0, 0, 0]
+    A = [] int{0, 0, 0}
     K = 1
 the function should return [0, 0, 0]
 
 Given
 
-    A = [1, 2, 3, 4]
+    A = [] int {1, 2, 3, 4}
     K = 4
 the function should return [1, 2, 3, 4]
 
@@ -39,8 +39,6 @@ Assume that:
 N and K are integers within the range [0..100];
 each element of array A is an integer within the range [−1,000..1,000].
 In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
-
-Copyright 2009–2021 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 
 */
 
