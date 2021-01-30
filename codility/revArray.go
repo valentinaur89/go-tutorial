1fc85490e102313aa2be95acf4ceca7ed2754054
Anees Fatima Shaik <Anees@MacBook-Pro.local>
2021-01-30 06:49:23 -0700

    -reverse any type of array

--- codility/revArray.go
+++ codility/revArray.go
@@ -3,23 +3,31 @@ package main
 import "fmt"
 
 func main() {
+
+	i := make([]interface{}, 0, 0)
 	// odd list
-	odd := []int{10, 20, 30, 40, 50}
-
-	// evn list
-	evn := []int{10, 20, 30, 40}
-	fmt.Println("Before list:", odd)
-	revers(odd)
-	fmt.Println("odd list:", odd)
-	fmt.Println("Before list:", evn)
-	revers(evn)
-	fmt.Println("even list:", evn)
-
+	i = append(i, 10, "a", 30, 40)
+	fmt.Printf("Before ::%v ", i)
+	fmt.Printf("After::%v\n", revers(i))
+	i = i[:0]
+	i = append(i, 1, 2, 3, "c")
+	fmt.Printf("Before ::%v After::%v\n", i, reverseRec(i))
 }
 
-func revers(arr []int) {
+// with loop
+func revers(arr []interface{}) []interface{} {
 	l := len(arr)
 	for i := 0; i < l/2; i++ {
 		arr[i], arr[l-(i+1)] = arr[l-(i+1)], arr[i]
 	}
+	return arr
+}
+
+// with recurs
+func reverseRec(arr []interface{}) []interface{} {
+	if len(arr) == 0 {
+		return arr
+	}
+
+	return append(reverseRec(arr[1:]), arr[0])
 }
